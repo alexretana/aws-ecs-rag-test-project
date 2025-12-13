1152376e43207190f474c79181bddf5a0174e6e8
alexretana <alex.retana@live.com>
2025-12-13 17:18:24 -0500

    make terraform able to force destroy s3 bucket

--- terraform/modules/codepipeline/main.tf
+++ terraform/modules/codepipeline/main.tf
@@ -1,6 +1,7 @@
 # S3 Bucket for CodePipeline artifacts
 resource "aws_s3_bucket" "codepipeline" {
   bucket = "${var.project_name}-${var.environment}-codepipeline-${var.account_id}"
+  force_destroy = true
 
   tags = {
     Name = "${var.project_name}-${var.environment}-codepipeline"
